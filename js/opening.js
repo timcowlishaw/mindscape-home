86d13d14ed254ea1058d95e2c4f7161960191f42
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-07-01 13:22:41 +0200

    change forces and stuff

--- js/opening.js
+++ js/opening.js
@@ -14,7 +14,7 @@ d3.json("data/leaving.json").then(function(links) {
             nNodes.push(nodes[link.source.name]);
             link.source = nodes[link.source.name] ;
         }
-     
+
         if (nodes[link.target.name]){
             link.target = nodes[link.target.name] ;
         }
@@ -22,26 +22,26 @@ d3.json("data/leaving.json").then(function(links) {
             nodes[link.target.name] = {name: link.target};
             nNodes.push(nodes[link.target.name]);
             link.target = nodes[link.target.name] ;
-        } 
-    }); 
-     
+        }
+    });
+
     var svg = d3.select("forum")
         .append("svg")
         .attr("width", width)
-        .attr("height", height), 
+        .attr("height", height),
         g = svg.append("g")
 ;
-    
+
         // forces
     var r = d3.forceRadial(500),
         x = d3.forceX(width),
         y = d3.forceY(height),
-        charge = d3.forceCollide().radius(-550),
-        attract = d3.forceManyBody().strength(2),
+        charge = d3.forceCollide().radius(-1850),
+        attract = d3.forceManyBody().strength(10),
         center = d3.forceCenter(width/2, height/2),
-        collide = d3.forceCollide().radius(35).iterations(5),
+        collide = d3.forceCollide().radius(35).iterations(3),
         link = d3.forceLink(links).distance(20);
-     
+
     var simulation = d3.forceSimulation(nNodes).alphaTarget(0.5).velocityDecay(0.6)
         .force("charge", charge)
         .force("r", r)
@@ -50,7 +50,7 @@ d3.json("data/leaving.json").then(function(links) {
         .force("collide", collide)
         .force("center", center)
         .force("link", link);
-    
+
     var zoom = d3.zoom()
             .scaleExtent([1, 8])
             .on('zoom', function(event) {
@@ -101,40 +101,40 @@ d3.json("data/leaving.json").then(function(links) {
       var dx = parseFloat(d.target.x) - parseFloat(d.source.x);
       var dy = parseFloat(d.target.y) - parseFloat(d.source.y);
       var dr = Math.sqrt(dx * dx + dy * dy);
-      var r =  d.source.name.size*10; 
+      var r =  d.source.name.size*10;
       var xPad,
-          yPad; 
-         
+          yPad;
+
         if(d.target.x < d.source.x) {
             xPad = d.source.x - r;
         } else {
             xPad = d.source.x + r;
         }
-        
+
         if(d.target.y < d.source.y) {
             yPad = d.source.y + r;
         } else {
             yPad = d.source.y - r;
         }
-        
-        l = Math.sqrt(dx * dx + r * r);   
+
+        l = Math.sqrt(dx * dx + r * r);
         let tearWidth = 1.05;
-        let path = 
-        `M ${d.target.x} ${d.target.y}, 
-        Q ${xPad*tearWidth} ${yPad*tearWidth}, ${d.source.x} ${d.source.y}, 
+        let path =
+        `M ${d.target.x} ${d.target.y},
+        Q ${xPad*tearWidth} ${yPad*tearWidth}, ${d.source.x} ${d.source.y},
         T ${d.target.x} ${d.target.y},
         Z`;
-        
+
     return path;
     }
 
     function transform(d) {
       return "translate(" + d.x + "," + d.y + ")";
     }
-    
+
     simulation.on("tick", tick);
     zoom(svg);
-    
+
 });
 
 // drag functions
@@ -144,7 +144,7 @@ function dragstarted(d) {
 
 function dragged(event, d) {
     d.x = event.x;
-    d.y = event.y;    
+    d.y = event.y;
   d3.select(this).select("text").attr("transform", d=> "translate("+[d.x,d.y]+")" );
 }
 
